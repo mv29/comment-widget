05369339fb5a926a85d6aae4e0025102d92858d2
mv29 <mrinal29verma@gmail.com>
2022-10-26 19:07:47 +0530

    removing fireBase credentials

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsHZDUCZqmkwCYdETVkKZXpykqY33IphU",
-    authDomain: "comment-widget-ff3cb.firebaseapp.com",
-    projectId: "comment-widget-ff3cb",
-    storageBucket: "comment-widget-ff3cb.appspot.com",
-    messagingSenderId: "350738105853",
-    appId: "1:350738105853:web:ae51832f4df16c52f64594"
   };
 
 // Initialize Firebase and Firestore
